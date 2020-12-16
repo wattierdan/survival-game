1bfbbf5fd61fa669a1ec0464665007d36158e6fb
Dan <wattierdan@live.com>
2020-12-16 12:20:40 -0800

    adds startGame function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var hasAname = false
 
 let newCharacter = []
 
-async function readyToStart() {
+function readyToStart() {
     inquirer  
         .prompt([
 
@@ -33,7 +33,7 @@ async function readyToStart() {
                     break
 
                 case 'No':
-                    console.log(`Ok, well come back some other time.....see you soon ${character.name}.`)
+                    console.log(`Ok, well come back some other time.....see you soon ${newCharacter[0].name}.`)
                     break
 
 
@@ -41,6 +41,67 @@ async function readyToStart() {
 
         })
 }
+
+
+function startGame() {
+    inquirer  
+    .prompt([
+
+        {
+            type: 'list',
+            name: 'readyToStart',
+            message: "You awake on a beach with no recollection of how you arrived here. You scan up and down the shore but there is no sign of human activity. Behind you is nothing but vast open ocean a far as the eye can see and in front of you lies a seemingly impenetrable jungle emanating an array of screaches and howls from its animal inhabitance. What would you like to do?",
+            choices: [
+                'Walk North along the beach',
+                'Walk South along the beach',
+                'Head into the jungle',
+                'Turn around and start swimming into the ocean'
+            ]
+        }
+
+    ]).then(userChoice => {
+        switch (userChoice.readyToStart) {
+
+            case 'Walk North along the beach':
+                walkNorth()
+                break
+
+            case 'Walk South along the beach':
+                walkSouth()
+                break
+
+            case 'Head into the jungle':
+                intoJungle()
+                break
+
+            case 'Turn around and start swimming into the ocean':
+                startSwimming()
+                break
+
+
+        }
+
+    })
+}
+
+
+function startSwimming() {
+    const outcomes = [
+
+        'you turn around and start to swim into the ocean. You make it about 100 yards from the beach when all of the sudden a great white shark emerges from the depths and rips you to shreds. GAME OVER.',
+
+        'you turn around and start to swim into the ocean. you swim and swim and just as the shore vanishes into the distance you beguin to get a side cramp with no other sight of land you turn around and start to swim back towards the beach. Half way back to the beach you become so exhuasted you are fighting to keep above the water. within 50 yards of the shore you cannot go on and drown. GAME OVER.',
+
+        'you turn around and start to swim into the ocean but are immediately eatten by a shark. GAME OVER.'
+    ]
+
+    const outcome = outcomes[Math.floor(Math.random() * outcomes.length)];
+
+    console.log(outcome)
+
+}
+
+
 
 
 Init()
